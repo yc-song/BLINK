mlp_with_bert backprop error fixed

blink/biencoder/eval_biencoder.py
@@ -253,11 +253,14 @@ def main(params):
             logger.info("Loading pre-generated candidate encode path.")
             candidate_encoding = torch.load(cand_encode_path+"/cand_enc_"+params["mode"]+".pt")
             candidate_cls=torch.load(cand_encode_path+"/cand_enc_"+params["mode"]+"_cls.pt")
-            cand_encode_late_interaction=torch.load(cand_encode_path+"/cand_enc_"+params["mode"]+"_late_interaction.pt")
+            if params["architecture"] == "mlp_with_som":
+                cand_encode_late_interaction = torch.load(cand_encode_path+"/cand_enc_"+params["mode"]+"_late_interaction.pt")
+            else:
+                cand_encode_late_interaction = None
         except:
             logger.info("Loading failed. Generating candidate encoding.")
 
-    if candidate_encoding is None or cand_encode_late_interaction is None:
+    if candidate_encoding is None or (cand_encode_late_interaction is None and params["architecture"] == "mlp_with_som"):
         candidate_encoding, candidate_cls, cand_encode_late_interaction = encode_candidate(
             reranker,
             candidate_pool,
@@ -267,6 +270,7 @@ def main(params):
             is_zeshel=params.get("zeshel", None)
             
         )
+
 
         if cand_encode_path is not None:
                 # Save candidate encoding to avoid re-compute

blink/biencoder/nn_prediction.py
@@ -59,7 +59,7 @@ def get_topk_predictions(
         candidate_pool_tensor = None
         # Stacking cand_encode_list and candidate_pool to one tensor
         for i in range(1, len(cand_encode_list)):
-            cumulative_idx.append(cumulative_idx[i-1]+cand_encode_list[i+src_minus].size(0))
+            cumulative_idx.append(cumulative_idx[i-1]+cand_encode_list[i-1+src_minus].size(0))
                 # World 0, 1, 2... have been concatenated in tensor [tensors of world 0, tensors of world 1, tensors of world 2, ...]
     else:
         # only one domain
@@ -163,7 +163,6 @@ def get_topk_predictions(
                 #     nn_context = torch.cat([nn_context, embedding_late_interaction_ctxt], dim=0)
             else:
                 nn_context.append(embedding_ctxt[i].cpu().tolist())#(1024)
-            
             nn_idxs.append([x + cumulative_idx[srcs[i].item()-src_minus] for x in inds.tolist()])
             nn_scores.append(value.tolist())
             nn_labels.append(pointer)

blink/crossencoder/crossencoder.py
@@ -102,8 +102,10 @@ class CrossEncoderRanker(torch.nn.Module):
         # init model
         self.build_model()
         if params["path_to_model"] is not None:
+            print("load")
             self.load_model(params["path_to_model"])
-
+        else:
+            print("not loaded")
         self.model = self.model.to(self.device)
         self.data_parallel = params.get("data_parallel")
         if self.data_parallel:
@@ -186,6 +188,7 @@ class MlpwithBiEncoderModule(BiEncoderModule):
         self.device = torch.device(
             "cuda" if torch.cuda.is_available() and not params["no_cuda"] else "cpu"
         )
+        self.mlpmodule = MlpModule(self.params).to(self.device)
     
     def forward(
         self, token_idx_ctxt, segment_idx_ctxt, mask_ctxt, token_idx_cands, segment_idx_cands, mask_cands
@@ -201,14 +204,14 @@ class MlpwithBiEncoderModule(BiEncoderModule):
             embedding_cands, _, _ = self.cand_encoder(
                 token_idx_cands, segment_idx_cands, mask_cands, data_type="candidate"
             )
+        score = self.mlpmodule(torch.cat((embedding_ctxt.unsqueeze(dim = 1), embedding_cands.unsqueeze(dim = 1)), dim=1).unsqueeze(dim = 0))
 
-        return embedding_ctxt, embedding_cands
+        return score
 
 class MlpwithBiEncoderRanker(CrossEncoderRanker): 
     def __init__(self, params, shared=None):
         super(MlpwithBiEncoderRanker, self).__init__(params)
         self.params = params
-        self.mlpmodule = MlpModule(self.params).to(self.device)
     def build_model(self):
         self.model = MlpwithBiEncoderModule(self.params, self.tokenizer)
     def load_model(self, fname, cpu=False):
@@ -231,11 +234,10 @@ class MlpwithBiEncoderRanker(CrossEncoderRanker):
             text_vecs_cands.int(), self.NULL_IDX, context_len,
         )
         # Get BERT embeddings
-        embedding_ctxt, embedding_cands = self.model(
+        score = self.model(
             token_idx_ctxt, segment_idx_ctxt, mask_ctxt, token_idx_cands, segment_idx_cands, mask_cands
         )
         # Take them as input of MLP layers
-        score = self.mlpmodule(torch.cat((embedding_ctxt.unsqueeze(dim = 1), embedding_cands.unsqueeze(dim = 1)), dim=1).unsqueeze(dim = 0))
         return score.view(-1, num_cand)
 
 class MlpwithSOMModule(nn.Module):
@@ -246,6 +248,10 @@ class MlpwithSOMModule(nn.Module):
     def forward(self, context):
         entity = context[:,:,1,:,:]
         context = context[:,:,0,:,:] #(max_length, embedding_dimension)
+        batch_size = entity.size(0)
+        top_k = entity.size(1)
+        max_length = entity.size(2)
+        embedding_dimension = entity.size(3)
         entity = entity.reshape(-1, max_length, embedding_dimension)
         context = context.reshape(-1, max_length, embedding_dimension)
         # perform batch-wise dot product using torch.bmm
@@ -267,7 +273,7 @@ class MlpwithSOMRanker(CrossEncoderRanker):
         super(MlpwithSOMRanker, self).__init__(params)
     def build_model(self):
         self.model = MlpwithSOMModule(self.params)
-    def forward(self, input, label_input, context_length):
+    def forward(self, input, label_input, context_length, evaluate = False):
         scores = self.model(input)
         loss = F.cross_entropy(scores, label_input)
         return loss, scores

blink/crossencoder/train_cross.py
@@ -43,9 +43,8 @@ logger = None
 def modify(context_input, candidate_input, params, world, idxs, mode = "train", wo64 = True):
     device = torch.device('cuda')
     # get values of candidates first
-    candidate_input = candidate_input.to(device)
-    candidate_input = candidate_input[idxs].squeeze(dim = 0).to(device)
-    print("0", candidate_input.shape)
+    # candidate_input = candidate_input.to(device)
+    candidate_input = candidate_input[idxs.cpu()].squeeze(dim = 0).to(device)
     top_k=params["top_k"]
     ## context_input shape: (Size ,1024) e.g.  (10000, 1024)
     ## candidate_input shape: (Size, 65, 1024) e.g. (10000, 65, 1024)
@@ -70,9 +69,7 @@ def modify(context_input, candidate_input, params, world, idxs, mode = "train", 
     else: 
         if params["architecture"] == "mlp_with_som":
             context_input =  context_input.unsqueeze(dim=1).expand(-1,top_k,-1,-1).to(device)
-            context_input = torch.stack((context_input, candidate_input), dim = 2)
-            print("1", context_input.shape)
-            raise("error")
+            new_input = torch.stack((context_input, candidate_input), dim = 2)
         else:
             context_input=context_input.unsqueeze(dim=1).expand(-1,top_k,-1).to(device)
             if params["architecture"] =="mlp" or params["architecture"]=="special_token" or params["architecture"]=="mlp_with_bert" or params["architecture"]=="baseline":
@@ -124,7 +121,7 @@ def evaluate(reranker, eval_dataloader, device, logger, context_length, candidat
         batch = tuple(t.to(device) for t in batch)
         context_input = batch[0]
         label_input = batch[1]
-        if params["top_k"]>100:
+        if params["top_k"]>100 or params["architecture"] == "mlp_with_som":
             world = batch[2]
             idxs = batch[4]
             context_input = modify(context_input, candidate_input, params, world, idxs, mode = "train", wo64 = params["without_64"])
@@ -245,9 +242,9 @@ def main(params):
     params["train_size"] = int(params["train_size"]/train_split)
     for i in range(train_split):
         if train_split == 1:
-            fname = os.path.join(params["data_path"], "train_{}.t7".format(params["architecture"]))
+            fname = os.path.join(params["data_path"], "valid_{}.t7".format(params["architecture"]))
         else:
-            fname = os.path.join(params["data_path"], "train_{}_{}.t7".format(params["architecture"], i))
+            fname = os.path.join(params["data_path"], "valid_{}_{}.t7".format(params["architecture"], i))
         if not os.path.isfile(fname):
             if params["architecture"] == "mlp":
                 if train_split == 1:
@@ -277,7 +274,7 @@ def main(params):
             label_input = torch.cat((label_input, train_data["labels"][:params["train_size"]]), dim = 0)
             bi_encoder_score = torch.cat((bi_encoder_score, train_data["nn_scores"][:params["train_size"]]), dim = 0)
             if params["zeshel"]:
-                src_input = train_data['worlds'][:params["train_size"]]
+                src_input = torch.cat((src_input, train_data['worlds'][:params["train_size"]]), dim = 0)
     # Init model
     if params["architecture"]=="mlp":
         reranker= MlpModel(params)
@@ -291,7 +288,7 @@ def main(params):
     model = reranker.model
     if reranker.n_gpu > 0:
         torch.cuda.manual_seed_all(seed)
-    if params["architecture"] == "mlp":
+    if params["architecture"] == "mlp" or params["architecture"] == "mlp_with_som":
         if params["optimizer"]=="Adam":
             optimizer = optim.Adam(model.parameters(), lr=params["learning_rate"])
         elif params["optimizer"]=="AdamW":
@@ -387,12 +384,6 @@ def main(params):
     if params["top_k"]<100 and params["architecture"] != "mlp_with_som":
         context_input = modify(context_input, candidate_input_train, params, src_input, idxs, mode = "train", wo64 = params["without_64"])
     if params["zeshel"]:
-        print(context_input.shape)
-        print(label_input.shape)
-        print(src_input.shape)
-        print(bi_encoder_score.shape)
-        print(idxs.shape)
-
         train_tensor_data = TensorDataset(context_input, label_input, src_input, bi_encoder_score, idxs)
         
     else:
@@ -605,12 +596,13 @@ def main(params):
                     model.parameters(), params["max_grad_norm"]
                 )
                 optimizer.step()
+
                 if params["architecture"]=="special_token" or params["architecture"]=="raw_context_text" or params["architecture"]=="mlp_with_bert":
                     scheduler.step()
                 optimizer.zero_grad()
-            for i, param in enumerate(list(reranker.named_parameters())):
-                print(param)
-                print(list(reranker.parameters())[i].grad)
+ #               for i, param in enumerate(list(reranker.named_parameters())):
+  #                  print(param)
+  #                  print(list(reranker.parameters())[i].grad)
             save_interval=500
             if not step % save_interval and params["save"]:
                 logger.info("***** Saving fine - tuned model *****")
@@ -670,7 +662,7 @@ def main(params):
             "params/epoch": epoch_idx
             })
             val_loss_sum = 0
-        if params["architecture"] != "raw_context_text" and params["architecture"] != "mlp_with_bert":
+        if params["architecture"] != "raw_context_text" and params["architecture"] != "mlp_with_bert" and params["train_size"]==-1:
             logger.info("Evaluation on the training dataset")
             train_acc=evaluate(
                 reranker,